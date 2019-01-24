866a1403fbb8fb4dec1a4abc826d410c612f1370
Tomohito YABU <tomotomo.y.1212@gmail.com>
2019-01-25 00:29:13 +0900

    Add String() method and AST test (chapter 2.6.4).

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/tyabu12/monkey/token"
+import (
+	"bytes"
+
+	"github.com/tyabu12/monkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -26,6 +31,16 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
 
 type ExpressionStatement struct {
@@ -58,5 +73,44 @@ type Identifier struct {
 	Value string
 }
 
-func (i *Identifier) statementNode()       {}
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (i *Identifier) String() string { return i.Value }
