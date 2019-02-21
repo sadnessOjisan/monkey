a3df39f3321ac8273cc20580df631b0f7d50646a
Tomohito YABU <tomotomo.y.1212@gmail.com>
2019-02-22 00:07:42 +0900

    Add integer evaluator (chapter 3.5.2).

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/tyabu12/monkey/evaluator"
 	"github.com/tyabu12/monkey/lexer"
 	"github.com/tyabu12/monkey/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
