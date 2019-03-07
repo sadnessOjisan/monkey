ddca57e06d16fba3b7a785ec857cd9656c3c671c
Tomohito YABU <tyabu1212@gmail.com>
2019-03-07 10:50:34 +0900

    Add tests about object.

--- object/object_test.go
+++ object/object_test.go
@@ -2,6 +2,64 @@ package object
 
 import "testing"
 
+func TestObjectType(t *testing.T) {
+	tests := []struct {
+		obj      Object
+		expected ObjectType
+	}{
+		{&Integer{}, INTEGER_OBJ},
+		{&String{}, STRING_OBJ},
+		{&Boolean{}, BOOLEAN_OBJ},
+		{&Null{}, NULL_OBJ},
+		{&Array{}, ARRAY_OBJ},
+		{&Hash{}, HASH_OBJ},
+		{&Error{}, ERROR_OBJ},
+		{&ReturnValue{}, RETURN_VALUE_OBJ},
+		{&Function{}, FUNCTION_OBJ},
+		{&Builtin{}, BUILTIN_OBJ},
+	}
+
+	for _, tt := range tests {
+		if tt.obj.Type() != tt.expected {
+			t.Errorf("wrong object type. expected=%s, got=%s",
+				tt.obj.Type(), tt.expected)
+		}
+	}
+}
+
+func TestObjectInspect(t *testing.T) {
+	tests := []struct {
+		obj      Object
+		expected string
+	}{
+		{&Integer{Value: -4}, "-4"},
+		{&String{Value: "John Doe"}, "John Doe"},
+		{&Boolean{Value: true}, "true"},
+		{&Null{}, "null"},
+		{
+			&Array{
+				Elements: []Object{
+					&Integer{Value: 1},
+					&String{Value: "aaa"},
+					&Boolean{Value: true},
+				}},
+			"[1, aaa, true]",
+		},
+		// TODO: Hash
+		{&Error{Message: "foobar"}, "ERROR: foobar"},
+		// TODO: ReturnValue
+		// TODO: Function
+		{&Builtin{}, "builtin function"},
+	}
+
+	for _, tt := range tests {
+		if tt.obj.Inspect() != tt.expected {
+			t.Errorf("wrong object type. expected=%s, got=%s",
+				tt.obj.Inspect(), tt.expected)
+		}
+	}
+}
+
 func TestBooleanHashKey(t *testing.T) {
 	true1 := &Boolean{Value: true}
 	true2 := &Boolean{Value: true}
