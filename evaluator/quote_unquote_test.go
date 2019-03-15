4f564023a52b58450c67cd4148bd382218a67f93
Tomohito YABU <tyabu1212@gmail.com>
2019-03-15 12:14:45 +0900

    Add boolean and quote to AST node convertion.

--- evaluator/quote_unquote_test.go
+++ evaluator/quote_unquote_test.go
@@ -75,6 +75,23 @@ func TestQuoteUnquote(t *testing.T) {
 			`quote(unquote(4 + 4) + 8)`,
 			`(8 + 8)`,
 		},
+		{
+			`quote(unquote(true))`,
+			`true`,
+		},
+		{
+			`quote(unquote(true == false))`,
+			`false`,
+		},
+		{
+			`quote(unquote(quote(4 + 4)))`,
+			`(4 + 4)`,
+		},
+		{
+			`let quotedInfixExpression = quote(4 + 4);
+			quote(unquote(4 + 4) + unquote(quotedInfixExpression))`,
+			`(8 + (4 + 4))`,
+		},
 	}
 
 	for _, tt := range tests {
